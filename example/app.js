8fc399f4099be107cde0a3e9f242b6c2dfb471a1
kg0r0 <33596117+kg0r0@users.noreply.github.com>
2019-05-17 21:08:37 +0900

    Add example application (#6)
    * add template app
    
    * add package
    
    * add sample page
    
    * Added .npmignore
    
    * some fix

---
+++ example/app.js
@@ -0,0 +1,52 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session');
+var logger = require('morgan');
+var crypto = require('crypto');
+var index = require('../lib/src/index.js');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(cookieSession({
+  name: 'session',
+  keys: [crypto.randomBytes(32).toString('hex')],
+  maxAge: 24 * 60 * 60 * 1000
+}));
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/attestation/options', index.attestationOptions);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
