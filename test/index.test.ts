0d33b8caeb44921f7e40aa98e43ab2ee3f26c891
kg0r0 <kg0r0@yahoo.co.jp>
2019-05-18 14:33:23 +0900

    Add test

--- test/index.test.ts
+++ test/index.test.ts
@@ -45,3 +45,39 @@ describe('attestationOptions()', () => {
   })
 
 })
+
+describe('assertionOptions()', () => {
+  const app = express();
+
+  app.use(bodyParser.json());
+
+  app.use(cookieSession({
+    name: 'session',
+    keys: [crypto.randomBytes(32).toString('hex')],
+
+    maxAge: 24 * 60 * 60 * 1000
+  }))
+
+  app.use(cookieParser());
+
+  app.use('/', index.assertionOptions);
+
+  it('should return ok status', (done) => {
+    request(app)
+      .post('/')
+      .send({ username: 'johndoe@example.com', displayName: 'John Doe' })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done)
+  })
+
+  it('should return failed status', (done) => {
+    request(app)
+      .post('/')
+      .send({})
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, '{"status":"failed","errorMessage":"Request missing username field!"}', done)
+  })
+
+})
