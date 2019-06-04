efb6f45bdf0cc616da7c5e0aee002177c2d242b8
kg0r0 <33596117+kg0r0@users.noreply.github.com>
2019-06-04 22:50:39 +0900

    Add a function to verify attestation result (#12)
    * [fix]change istanbul to nyc (#4)
    
    * [fix]change istanbul to nyc
    
    * [fix]include path
    
    * [fix]rm include and all
    
    * Add a function to generate attestation options (#1)
    
    * generate options
    
    * add option
    
    * remove unused file
    
    * Add example application (#6)
    
    * add template app
    
    * add package
    
    * add sample page
    
    * Added .npmignore
    
    * some fix
    
    * add options
    
    * some fix
    
    * Create LICENSE (#8)
    
    * Adding Continuous Integration (#9)
    
    * .travis.yml
    
    * add code coverage
    
    * add badge
    
    * fixed attestation options
    
    * Add ESLint (#11)
    
    * [add]eslint settings
    
    * [fix]auto fix by eslint
    
    * [add]lint task in ci
    
    * [add].eslintrc
    
    * Fixed lint setting & Removed unused code (#13)
    
    * fix lint setting
    
    * Fixed lint error
    
    * fixed lint error
    
    * add params
    
    * add tls setting
    
    * fixed type of parameter
    
    * changed example app to temporary setting
    
    * fixed confilict
    
    * add registration

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,42 @@ import { NextFunction, Request, Response } from "express";
 const fido2lib = require("fido2-lib");
 const crypto = require("crypto");
 const base64url = require("base64url");
+const config = require("config");
+const database = require("./db");
+const str2ab = require("string-to-arraybuffer");
+const fido2MiddlewareConfig = config.get("fido2-middlewareConfig");
 
+interface AuthrInfo {
+  fmt: string;
+  publicKey: string;
+  counter: number;
+  credID: string;
+}
+
+/**
+ *
+ * @param len
+ */
 function randomBase64URLBuffer(len: number) {
   len = len || 32;
   const buff = crypto.randomBytes(len);
 
   return base64url(buff);
+}
+
+function preformatResultReq(resultReq: Request) {
+  resultReq.body.rawId = str2ab(resultReq.body.rawId);
+  resultReq.body.id = str2ab(resultReq.body.id);
+  return resultReq;
+}
+
+/**
+ * Takes string and tries to verify base64 url encoded.
+ * @param  {String} str
+ * @return {Boolean}
+ */
+function isBase64UrlEncoded(str: String) {
+  return !!str.match(/^[A-Za-z0-9\-_]+={0,2}$/);
 }
 
 /**
@@ -25,12 +55,32 @@ async function attestationOptions(req: Request, res: Response) {
     });
   }
 
-  // TODO
   let excludeCredentials;
+  if (!fido2MiddlewareConfig.db) {
+    if (database[req.body.username] && database[req.body.username].registered) {
+      excludeCredentials = [
+        {
+          type: "public-key",
+          id: database[req.body.username].authenticatorp[0].credID
+        }
+      ];
+    } else {
+      database[req.body.username] = {
+        name: req.body.displayName,
+        registerd: false,
+        id: randomBase64URLBuffer(32),
+        authenticators: []
+      };
+    }
+  }
 
-  const serve = new fido2lib.Fido2Lib();
-  const options = await serve.attestationOptions();
-
+  const fido2Lib = new fido2lib.Fido2Lib(fido2MiddlewareConfig.fido2lib);
+  const options = await fido2Lib.attestationOptions().catch((err: Error) => {
+    return res.json({
+      status: "failed",
+      errorMessage: err.message
+    });
+  });
   options.status = "ok";
   options.errorMessage = "";
   options.extensions = req.body.extensions;
@@ -55,8 +105,78 @@ async function attestationOptions(req: Request, res: Response) {
  * @param {Function} next - Express next middleware function
  * @returns {undefined}
  */
-function attestationResult(req: Request, res: Response, next: NextFunction) {
-  next();
+async function attestationResult(req: Request, res: Response) {
+  if (
+    !req.body ||
+    !req.body.id ||
+    !req.body.rawId ||
+    !req.body.response ||
+    !req.body.type
+  ) {
+    return res.json({
+      status: "failed",
+      errorMessage:
+        "Response missing one or more of id/rawId/response/type fields"
+    });
+  }
+
+  if (req.body.type !== "public-key") {
+    return res.json({
+      status: "failed",
+      errorMessage: "type is not public-key!"
+    });
+  }
+
+  if (!isBase64UrlEncoded(req.body.id)) {
+    return res.json({
+      status: "failed",
+      errorMessage: "Invalid id!"
+    });
+  }
+
+  const fido2Lib = new fido2lib.Fido2Lib();
+  let expected = {
+    challenge: String,
+    origin: String,
+    factor: String
+  };
+  if (req.session) {
+    expected.challenge = req.session.challenge;
+  }
+  expected.origin = fido2MiddlewareConfig.origin || "localhost";
+  expected.factor = fido2MiddlewareConfig.factor || "either";
+  const requestBody = preformatResultReq(req).body;
+  const result = await fido2Lib
+    .attestationResult(requestBody, expected)
+    .catch((err: Error) => {
+      return res.json({
+        status: "failed",
+        errorMessage: err.message
+      });
+    });
+
+  if (!result) {
+    return res.json({
+      status: "failed",
+      errorMessage: "Can not authenticate signature!"
+    });
+  }
+  const authrInfo: AuthrInfo = {
+    fmt: result.authnrData.get("fmt"),
+    publicKey: result.authnrData.get("credentialPublicKeyPem"),
+    counter: result.authnrData.get("counter"),
+    credID: result.authnrData.get("credId")
+  };
+
+  if (req.session) {
+    database[req.session.username].authenticators.push(authrInfo);
+    database[req.session.username].registerd = true;
+  }
+
+  return res.json({
+    status: "ok",
+    errorMessage: ""
+  });
 }
 
 /**
