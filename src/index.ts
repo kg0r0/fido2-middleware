716c2195a432a8ae6b5ad3d58b4b3eea48cb3b47
kg0r0 <kg0r0@yahoo.co.jp>
2019-06-11 21:10:21 +0900

    fix lint error

--- src/index.ts
+++ src/index.ts
@@ -188,29 +188,28 @@ async function attestationResult(req: Request, res: Response) {
  *
  * @param {Object} req - Express request object
  * @param {Object} res - Express response object
- * @param {Function} next - Express next middleware function
  * @returns {undefined}
  */
-async function assertionOptions(req: Request, res: Response, next: NextFunction) {
+async function assertionOptions(req: Request, res: Response) {
   if (!req.body || !req.body.username) {
     return res.json({
-      'status': 'failed',
-      'errorMessage': 'Request missing username field!'
-    })
+      status: "failed",
+      errorMessage: "Request missing username field!"
+    });
   }
   const serve = new fido2lib.Fido2Lib();
   const options = await serve.assertionOptions();
-  options.status = 'ok';
-  options.errorMessage = '';
+  options.status = "ok";
+  options.errorMessage = "";
   options.extensions = req.body.extensions;
   options.challenge = randomBase64URLBuffer(32);
-  options.userVerification = req.body.userVerification || 'preferred';
+  options.userVerification = req.body.userVerification || "preferred";
   if (req.session) {
     req.session.challenge = options.challenge;
     req.session.username = req.body.username;
     req.session.userVerification = options.userVerification;
   }
-  return res.json(options)
+  return res.json(options);
 }
 
 /**
--- test/index.test.ts
+++ test/index.test.ts
@@ -39,45 +39,53 @@ describe("attestationOptions()", () => {
     request(app)
       .post("/")
       .send({})
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200, '{"status":"failed","errorMessage":"Request missing display name or username field!"}', done)
-  })
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(
+        200,
+        '{"status":"failed","errorMessage":"Request missing display name or username field!"}',
+        done
+      );
+  });
+});
 
-})
-
-describe('assertionOptions()', () => {
+describe("assertionOptions()", () => {
   const app = express();
 
   app.use(bodyParser.json());
 
-  app.use(cookieSession({
-    name: 'session',
-    keys: [crypto.randomBytes(32).toString('hex')],
+  app.use(
+    cookieSession({
+      name: "session",
+      keys: [crypto.randomBytes(32).toString("hex")],
 
-    maxAge: 24 * 60 * 60 * 1000
-  }))
+      maxAge: 24 * 60 * 60 * 1000
+    })
+  );
 
   app.use(cookieParser());
 
-  app.use('/', index.assertionOptions);
+  app.use("/", index.assertionOptions);
 
-  it('should return ok status', (done) => {
+  it("should return ok status", done => {
     request(app)
-      .post('/')
-      .send({ username: 'johndoe@example.com', displayName: 'John Doe' })
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200, done)
-  })
+      .post("/")
+      .send({ username: "johndoe@example.com", displayName: "John Doe" })
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(200, done);
+  });
 
-  it('should return failed status', (done) => {
+  it("should return failed status", done => {
     request(app)
-      .post('/')
+      .post("/")
       .send({})
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200, '{"status":"failed","errorMessage":"Request missing username field!"}', done)
-  })
-
-})
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(
+        200,
+        '{"status":"failed","errorMessage":"Request missing username field!"}',
+        done
+      );
+  });
+});
