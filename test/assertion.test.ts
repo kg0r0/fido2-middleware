d271da0b6d6693e0cc9508a3dddbafec4320a7fa
kg0r0 <kg0r0@yahoo.co.jp>
2019-07-11 03:18:50 +0900

    fixed lint error

--- test/assertion.test.ts
+++ test/assertion.test.ts
@@ -1,27 +1,23 @@
 import { describe, it } from "mocha";
-import { assertionOptions } from "../src/assertion"
-import { mockReq } from "sinon-express-mock"
+import { assertionOptions } from "../src/assertion";
+import { mockReq } from "sinon-express-mock";
 import { expect } from "chai";
 
 describe("attestationOptions()", () => {
   it("should return ok status", async () => {
     const request = {
       body: {
-        username: "johndoe@example.com",
+        username: "johndoe@example.com"
       },
-      session: {
-      }
-
-    }
-    const requestMock = mockReq(request)
-    const options = await assertionOptions(requestMock)
+      session: {}
+    };
+    const requestMock = mockReq(request);
+    const options = await assertionOptions(requestMock);
     expect(options.status).to.equal("ok");
   });
 
-  it("should return failed status", async() => {
-    const request = {
-
-    };
+  it("should return failed status", async () => {
+    const request = {};
     const requestMock = mockReq(request);
     const options = await assertionOptions(requestMock);
     expect(options.status).to.equal("failed");
--- test/attestation.test.ts
+++ test/attestation.test.ts
@@ -1,6 +1,6 @@
 import { describe, it } from "mocha";
-import { attestationOptions } from "../src/attestation"
-import { mockReq } from "sinon-express-mock"
+import { attestationOptions } from "../src/attestation";
+import { mockReq } from "sinon-express-mock";
 import { expect } from "chai";
 
 describe("attestationOptions()", () => {
@@ -10,19 +10,15 @@ describe("attestationOptions()", () => {
         username: "johndoe@example.com",
         displayName: "John Doe"
       },
-      session: {
-      }
-
-    }
-    const requestMock = mockReq(request)
-    const options = await attestationOptions(requestMock)
+      session: {}
+    };
+    const requestMock = mockReq(request);
+    const options = await attestationOptions(requestMock);
     expect(options.status).to.equal("ok");
   });
 
-  it("should return failed status", async() => {
-    const request = {
-
-    };
+  it("should return failed status", async () => {
+    const request = {};
     const requestMock = mockReq(request);
     const options = await attestationOptions(requestMock);
     expect(options.status).to.equal("failed");
