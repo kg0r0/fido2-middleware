1e9c7b403f51abf06526a7107b3d8da8f5e83dc6
kg0r0 <33596117+kg0r0@users.noreply.github.com>
2019-07-17 21:59:58 +0900

    Add test case for attestation operation and assertion operation (#44)

--- test/assertion.test.ts
+++ test/assertion.test.ts
@@ -1,5 +1,5 @@
 import { describe, it } from "mocha";
-import { assertionOptions, findAuthr } from "../src/assertion";
+import { assertionOptions, assertionResult, findAuthr } from "../src/assertion";
 import { mockReq } from "sinon-express-mock";
 import { expect } from "chai";
 
@@ -23,6 +23,85 @@ describe("assertionOptions()", () => {
       await assertionOptions(requestMock);
     } catch (e) {
       expect(e.message).to.equal("Request missing username field!");
+    }
+  });
+});
+
+describe("assertionResult()", () => {
+  it("should return 'Response missing one or more of id/rawId/response/type fields'", async () => {
+    const request = {};
+    const requestMock = mockReq(request);
+    try {
+      await assertionResult(requestMock);
+    } catch (e) {
+      expect(e.message).to.equal(
+        "Response missing one or more of id/rawId/response/type fields"
+      );
+    }
+  });
+
+  it("should return 'type is not public-key!'", async () => {
+    const request = {
+      body: {
+        id:
+          "LFdoCFJTyB82ZzSJUHc-c72yraRc_1mPvGX8ToE8su39xX26Jcqd31LUkKOS36FIAWgWl6itMKqmDvruha6ywA",
+        rawId:
+          "LFdoCFJTyB82ZzSJUHc-c72yraRc_1mPvGX8ToE8su39xX26Jcqd31LUkKOS36FIAWgWl6itMKqmDvruha6ywA",
+        response: {},
+        type: "test"
+      }
+    };
+    const requestMock = mockReq(request);
+    try {
+      await assertionResult(requestMock);
+    } catch (e) {
+      expect(e.message).to.equal("type is not public-key!");
+    }
+  });
+
+  it("should return 'AuthenticatorData is missing'", async () => {
+    const request = {
+      body: {
+        id:
+          "LFdoCFJTyB82ZzSJUHc-c72yraRc_1mPvGX8ToE8su39xX26Jcqd31LUkKOS36FIAWgWl6itMKqmDvruha6ywA",
+        rawId:
+          "LFdoCFJTyB82ZzSJUHc-c72yraRc_1mPvGX8ToE8su39xX26Jcqd31LUkKOS36FIAWgWl6itMKqmDvruha6ywA",
+        response: {},
+        type: "public-key"
+      }
+    };
+    const requestMock = mockReq(request);
+    try {
+      await assertionResult(requestMock);
+    } catch (e) {
+      expect(e.message).to.equal("AuthenticatorData is missing");
+    }
+  });
+
+  it("should return 'Challenges don't match!'", async () => {
+    const request = {
+      body: {
+        id:
+          "LFdoCFJTyB82ZzSJUHc-c72yraRc_1mPvGX8ToE8su39xX26Jcqd31LUkKOS36FIAWgWl6itMKqmDvruha6ywA",
+        rawId:
+          "LFdoCFJTyB82ZzSJUHc-c72yraRc_1mPvGX8ToE8su39xX26Jcqd31LUkKOS36FIAWgWl6itMKqmDvruha6ywA",
+        response: {
+          authenticatorData:
+            "SZYN5YgOjGh0NBcPZHZgW4_krrmihjLHmVzzuoMdl2MBAAAAAA",
+          signature:
+            "MEYCIQCv7EqsBRtf2E4o_BjzZfBwNpP8fLjd5y6TUOLWt5l9DQIhANiYig9newAJZYTzG1i5lwP-YQk9uXFnnDaHnr2yCKXL",
+          userHandle: "",
+          clientDataJSON:
+            "eyJjaGFsbGVuZ2UiOiJ4ZGowQ0JmWDY5MnFzQVRweTBrTmM4NTMzSmR2ZExVcHFZUDh3RFRYX1pFIiwiY2xpZW50RXh0ZW5zaW9ucyI6e30sImhhc2hBbGdvcml0aG0iOiJTSEEtMjU2Iiwib3JpZ2luIjoiaHR0cDovL2xvY2FsaG9zdDozMDAwIiwidHlwZSI6IndlYmF1dGhuLmdldCJ9"
+        },
+        type: "public-key"
+      }
+    };
+    const requestMock = mockReq(request);
+    try {
+      await assertionResult(requestMock);
+    } catch (e) {
+      expect(e.message).to.equal("Challenges don't match!");
     }
   });
 });
--- test/attestation.test.ts
+++ test/attestation.test.ts
@@ -1,5 +1,5 @@
 import { describe, it } from "mocha";
-import { attestationOptions } from "../src/attestation";
+import { attestationOptions, attestationResult } from "../src/attestation";
 import { mockReq } from "sinon-express-mock";
 import { expect } from "chai";
 
@@ -25,6 +25,46 @@ describe("attestationOptions()", () => {
     } catch (e) {
       expect(e.message).to.equal(
         "Request missing display name or username field!"
+      );
+    }
+  });
+});
+
+describe("attestationResult()", () => {
+  it("should return 'Response missing one or more of id/rawId/response/type fields'", async () => {
+    const request = {};
+    const requestMock = mockReq(request);
+    try {
+      await attestationResult(requestMock);
+    } catch (e) {
+      expect(e.message).to.equal(
+        "Response missing one or more of id/rawId/response/type fields"
+      );
+    }
+  });
+
+  it("should return 'expectation did not contain value for 'challenge''", async () => {
+    const request = {
+      body: {
+        id:
+          "LFdoCFJTyB82ZzSJUHc-c72yraRc_1mPvGX8ToE8su39xX26Jcqd31LUkKOS36FIAWgWl6itMKqmDvruha6ywA",
+        rawId:
+          "LFdoCFJTyB82ZzSJUHc-c72yraRc_1mPvGX8ToE8su39xX26Jcqd31LUkKOS36FIAWgWl6itMKqmDvruha6ywA",
+        response: {
+          clientDataJSON:
+            "eyJjaGFsbGVuZ2UiOiJOeHlab3B3VktiRmw3RW5uTWFlXzVGbmlyN1FKN1FXcDFVRlVLakZIbGZrIiwiY2xpZW50RXh0ZW5zaW9ucyI6e30sImhhc2hBbGdvcml0aG0iOiJTSEEtMjU2Iiwib3JpZ2luIjoiaHR0cDovL2xvY2FsaG9zdDozMDAwIiwidHlwZSI6IndlYmF1dGhuLmNyZWF0ZSJ9",
+          attestationObject:
+            "o2NmbXRoZmlkby11MmZnYXR0U3RtdKJjc2lnWEcwRQIgVzzvX3Nyp_g9j9f2B-tPWy6puW01aZHI8RXjwqfDjtQCIQDLsdniGPO9iKr7tdgVV-FnBYhvzlZLG3u28rVt10YXfGN4NWOBWQJOMIICSjCCATKgAwIBAgIEVxb3wDANBgkqhkiG9w0BAQsFADAuMSwwKgYDVQQDEyNZdWJpY28gVTJGIFJvb3QgQ0EgU2VyaWFsIDQ1NzIwMDYzMTAgFw0xNDA4MDEwMDAwMDBaGA8yMDUwMDkwNDAwMDAwMFowLDEqMCgGA1UEAwwhWXViaWNvIFUyRiBFRSBTZXJpYWwgMjUwNTY5MjI2MTc2MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEZNkcVNbZV43TsGB4TEY21UijmDqvNSfO6y3G4ytnnjP86ehjFK28-FdSGy9MSZ-Ur3BVZb4iGVsptk5NrQ3QYqM7MDkwIgYJKwYBBAGCxAoCBBUxLjMuNi4xLjQuMS40MTQ4Mi4xLjUwEwYLKwYBBAGC5RwCAQEEBAMCBSAwDQYJKoZIhvcNAQELBQADggEBAHibGMqbpNt2IOL4i4z96VEmbSoid9Xj--m2jJqg6RpqSOp1TO8L3lmEA22uf4uj_eZLUXYEw6EbLm11TUo3Ge-odpMPoODzBj9aTKC8oDFPfwWj6l1O3ZHTSma1XVyPqG4A579f3YAjfrPbgj404xJns0mqx5wkpxKlnoBKqo1rqSUmonencd4xanO_PHEfxU0iZif615Xk9E4bcANPCfz-OLfeKXiT-1msixwzz8XGvl2OTMJ_Sh9G9vhE-HjAcovcHfumcdoQh_WM445Za6Pyn9BZQV3FCqMviRR809sIATfU5lu86wu_5UGIGI7MFDEYeVGSqzpzh6mlcn8QSIZoYXV0aERhdGFYxEmWDeWIDoxodDQXD2R2YFuP5K65ooYyx5lc87qDHZdjQQAAAAAAAAAAAAAAAAAAAAAAAAAAAEAsV2gIUlPIHzZnNIlQdz5zvbKtpFz_WY-8ZfxOgTyy7f3Ffbolyp3fUtSQo5LfoUgBaBaXqK0wqqYO-u6FrrLApQECAyYgASFYIPr9-YH8DuBsOnaI3KJa0a39hyxh9LDtHErNvfQSyxQsIlgg4rAuQQ5uy4VXGFbkiAt0uwgJJodp-DymkoBcrGsLtkI"
+        },
+        type: "public-key"
+      }
+    };
+    const requestMock = mockReq(request);
+    try {
+      await attestationResult(requestMock);
+    } catch (e) {
+      expect(e.message).to.equal(
+        "expectation did not contain value for 'challenge'"
       );
     }
   });
