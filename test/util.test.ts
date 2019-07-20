6a29a23932395784c5951c345a54e5a88e50b724
kg0r0 <33596117+kg0r0@users.noreply.github.com>
2019-07-20 22:34:35 +0900

    Add test for preformatAssertionResultReq() (#45)
    * add test
    
    * fix lint error

--- test/util.test.ts
+++ test/util.test.ts
@@ -57,286 +57,296 @@ describe("preformatAttestationResultReq()", () => {
 
 describe("preformatAssertionResultReq()", () => {
   const reqBody = {
-    id: "test id",
-    rawId: "test rawId",
-    response: {},
-    type: "test"
+    id:
+      "LFdoCFJTyB82ZzSJUHc-c72yraRc_1mPvGX8ToE8su39xX26Jcqd31LUkKOS36FIAWgWl6itMKqmDvruha6ywA",
+    rawId:
+      "LFdoCFJTyB82ZzSJUHc-c72yraRc_1mPvGX8ToE8su39xX26Jcqd31LUkKOS36FIAWgWl6itMKqmDvruha6ywA",
+    response: {
+      authenticatorData: "SZYN5YgOjGh0NBcPZHZgW4_krrmihjLHmVzzuoMdl2MBAAAAAA",
+      signature:
+        "MEYCIQCv7EqsBRtf2E4o_BjzZfBwNpP8fLjd5y6TUOLWt5l9DQIhANiYig9newAJZYTzG1i5lwP-YQk9uXFnnDaHnr2yCKXL",
+      userHandle: "",
+      clientDataJSON:
+        "eyJjaGFsbGVuZ2UiOiJ4ZGowQ0JmWDY5MnFzQVRweTBrTmM4NTMzSmR2ZExVcHFZUDh3RFRYX1pFIiwiY2xpZW50RXh0ZW5zaW9ucyI6e30sImhhc2hBbGdvcml0aG0iOiJTSEEtMjU2Iiwib3JpZ2luIjoiaHR0cDovL2xvY2FsaG9zdDozMDAwIiwidHlwZSI6IndlYmF1dGhuLmdldCJ9"
+    },
+    type: "public-key"
   };
   it("should return ArrayBuffer", () => {
     const body = preFormatAssertionResultReq(reqBody);
     expect(body.id instanceof ArrayBuffer).to.equal(true);
     expect(body.rawId instanceof ArrayBuffer).to.equal(true);
+    expect(body.response.authenticatorData instanceof ArrayBuffer).to.equal(
+      true
+    );
+  });
+});
+
+describe("isRequestBody()", () => {
+  const body = {
+    id: "id",
+    rawId: "rawId",
+    response: {},
+    type: "type"
+  };
+  it("should return true", () => {
+    expect(isRequestBody(body)).to.equal(true);
   });
 
-  describe("isRequestBody()", () => {
+  it("should return false", () => {
+    expect(isRequestBody({})).to.equal(false);
+  });
+});
+
+describe("ClientDataJSONValidater()", () => {
+  const request = {
+    body: {
+      username: "johndoe@example.com",
+      displayName: "John Doe"
+    },
+    session: {
+      challenge: "challenge"
+    }
+  };
+  const requestMock = mockReq(request);
+
+  it("should return 'ok'", () => {
+    const clinetDataJSON = {
+      challenge: "challenge",
+      origin: "https://localhost:3000",
+      type: "webauthn.get",
+      tokenBinding: ""
+    };
+    const result = assertionClientDataJSONValidator(
+      requestMock,
+      clinetDataJSON
+    );
+    expect(result).to.equal(true);
+  });
+
+  it("should return 'Challenges don't match!'", () => {
+    const clinetDataJSON = {
+      challenge: "dummy",
+      origin: "http://localhost:3000",
+      type: "webauthn.get",
+      tokenBinding: ""
+    };
+    try {
+      assertionClientDataJSONValidator(requestMock, clinetDataJSON);
+    } catch (e) {
+      expect(e.message).to.equal("Challenges don't match!");
+    }
+  });
+
+  it("should return 'Origins don't match!'", () => {
+    const clinetDataJSON = {
+      challenge: "challenge",
+      origin: "dummy",
+      type: "webauthn.get",
+      tokenBinding: ""
+    };
+    try {
+      assertionClientDataJSONValidator(requestMock, clinetDataJSON);
+    } catch (e) {
+      expect(e.message).to.equal("Origins don't match!");
+    }
+  });
+
+  it("should return 'Type don't match!'", () => {
+    const clinetDataJSON = {
+      challenge: "challenge",
+      origin: "https://localhost:3000",
+      type: "webauthn.create",
+      tokenBinding: ""
+    };
+    try {
+      assertionClientDataJSONValidator(requestMock, clinetDataJSON);
+    } catch (e) {
+      expect(e.message).to.equal("Type don't match!");
+    }
+  });
+
+  it("should return 'Token Binding don`t support!'", () => {
+    const clinetDataJSON = {
+      challenge: "challenge",
+      origin: "https://localhost:3000",
+      type: "webauthn.get",
+      tokenBinding: "tokenBinding"
+    };
+    try {
+      assertionClientDataJSONValidator(requestMock, clinetDataJSON);
+    } catch (e) {
+      expect(e.message).to.equal("Token Binding don`t support!");
+    }
+  });
+});
+
+describe("attestationResultReqValidator()", () => {
+  it("should return true", () => {
+    const body = {
+      id: "id",
+      rawId: "rawId",
+      response: {},
+      type: "public-key"
+    };
+    expect(attestationResultReqValidator(body)).to.equal(true);
+  });
+
+  it("should return 'Response missing one or more of id/rawId/response/type fields'", () => {
+    const body = {};
+    try {
+      attestationResultReqValidator(body);
+    } catch (e) {
+      expect(e.message).to.equal(
+        "Response missing one or more of id/rawId/response/type fields"
+      );
+    }
+  });
+
+  it("should return 'type is not public-key!'", () => {
     const body = {
       id: "id",
       rawId: "rawId",
       response: {},
       type: "type"
     };
-    it("should return true", () => {
-      expect(isRequestBody(body)).to.equal(true);
-    });
-
-    it("should return false", () => {
-      expect(isRequestBody({})).to.equal(false);
-    });
+    try {
+      attestationResultReqValidator(body);
+    } catch (e) {
+      expect(e.message).to.equal("type is not public-key!");
+    }
   });
 
-  describe("ClientDataJSONValidater()", () => {
-    const request = {
-      body: {
-        username: "johndoe@example.com",
-        displayName: "John Doe"
-      },
-      session: {
-        challenge: "challenge"
-      }
+  it("should return 'Invalid id!'", () => {
+    const body = {
+      id: "+/=",
+      rawId: "rawId",
+      response: {},
+      type: "public-key"
     };
-    const requestMock = mockReq(request);
+    try {
+      attestationResultReqValidator(body);
+    } catch (e) {
+      expect(e.message).to.equal("Invalid id!");
+    }
+  });
+});
 
-    it("should return 'ok'", () => {
-      const clinetDataJSON = {
-        challenge: "challenge",
-        origin: "https://localhost:3000",
-        type: "webauthn.get",
-        tokenBinding: ""
-      };
-      const result = assertionClientDataJSONValidator(
-        requestMock,
-        clinetDataJSON
+describe("assertionResultReqValidator()", () => {
+  it("should return true", () => {
+    const body = {
+      id: "id",
+      rawId: "rawId",
+      response: {
+        authenticatorData: "authenticatorData",
+        userHandle: "userHandle",
+        signature: "signature"
+      },
+      type: "public-key"
+    };
+    expect(assertionResultReqValidator(body)).to.equal(true);
+  });
+
+  it("should return 'Response missing one or more of id/rawId/response/type fields'", () => {
+    const body = {};
+    try {
+      assertionResultReqValidator(body);
+    } catch (e) {
+      expect(e.message).to.equal(
+        "Response missing one or more of id/rawId/response/type fields"
       );
-      expect(result).to.equal(true);
-    });
-
-    it("should return 'Challenges don't match!'", () => {
-      const clinetDataJSON = {
-        challenge: "dummy",
-        origin: "http://localhost:3000",
-        type: "webauthn.get",
-        tokenBinding: ""
-      };
-      try {
-        assertionClientDataJSONValidator(requestMock, clinetDataJSON);
-      } catch (e) {
-        expect(e.message).to.equal("Challenges don't match!");
-      }
-    });
-
-    it("should return 'Origins don't match!'", () => {
-      const clinetDataJSON = {
-        challenge: "challenge",
-        origin: "dummy",
-        type: "webauthn.get",
-        tokenBinding: ""
-      };
-      try {
-        assertionClientDataJSONValidator(requestMock, clinetDataJSON);
-      } catch (e) {
-        expect(e.message).to.equal("Origins don't match!");
-      }
-    });
-
-    it("should return 'Type don't match!'", () => {
-      const clinetDataJSON = {
-        challenge: "challenge",
-        origin: "https://localhost:3000",
-        type: "webauthn.create",
-        tokenBinding: ""
-      };
-      try {
-        assertionClientDataJSONValidator(requestMock, clinetDataJSON);
-      } catch (e) {
-        expect(e.message).to.equal("Type don't match!");
-      }
-    });
-
-    it("should return 'Token Binding don`t support!'", () => {
-      const clinetDataJSON = {
-        challenge: "challenge",
-        origin: "https://localhost:3000",
-        type: "webauthn.get",
-        tokenBinding: "tokenBinding"
-      };
-      try {
-        assertionClientDataJSONValidator(requestMock, clinetDataJSON);
-      } catch (e) {
-        expect(e.message).to.equal("Token Binding don`t support!");
-      }
-    });
+    }
   });
 
-  describe("attestationResultReqValidator()", () => {
-    it("should return true", () => {
-      const body = {
-        id: "id",
-        rawId: "rawId",
-        response: {},
-        type: "public-key"
-      };
-      expect(attestationResultReqValidator(body)).to.equal(true);
-    });
-
-    it("should return 'Response missing one or more of id/rawId/response/type fields'", () => {
-      const body = {};
-      try {
-        attestationResultReqValidator(body);
-      } catch (e) {
-        expect(e.message).to.equal(
-          "Response missing one or more of id/rawId/response/type fields"
-        );
-      }
-    });
-
-    it("should return 'type is not public-key!'", () => {
-      const body = {
-        id: "id",
-        rawId: "rawId",
-        response: {},
-        type: "type"
-      };
-      try {
-        attestationResultReqValidator(body);
-      } catch (e) {
-        expect(e.message).to.equal("type is not public-key!");
-      }
-    });
-
-    it("should return 'Invalid id!'", () => {
-      const body = {
-        id: "+/=",
-        rawId: "rawId",
-        response: {},
-        type: "public-key"
-      };
-      try {
-        attestationResultReqValidator(body);
-      } catch (e) {
-        expect(e.message).to.equal("Invalid id!");
-      }
-    });
+  it("should return 'type is not public-key!'", () => {
+    const body = {
+      id: "id",
+      rawId: "rawId",
+      response: {},
+      type: "type"
+    };
+    try {
+      assertionResultReqValidator(body);
+    } catch (e) {
+      expect(e.message).to.equal("type is not public-key!");
+    }
   });
 
-  describe("assertionResultReqValidator()", () => {
-    it("should return true", () => {
-      const body = {
-        id: "id",
-        rawId: "rawId",
-        response: {
-          authenticatorData: "authenticatorData",
-          userHandle: "userHandle",
-          signature: "signature"
-        },
-        type: "public-key"
-      };
-      expect(assertionResultReqValidator(body)).to.equal(true);
-    });
+  it("should return 'Invalid id!'", () => {
+    const body = {
+      id: "+/=",
+      rawId: "rawId",
+      response: {},
+      type: "public-key"
+    };
+    try {
+      assertionResultReqValidator(body);
+    } catch (e) {
+      expect(e.message).to.equal("Invalid id!");
+    }
+  });
 
-    it("should return 'Response missing one or more of id/rawId/response/type fields'", () => {
-      const body = {};
-      try {
-        assertionResultReqValidator(body);
-      } catch (e) {
-        expect(e.message).to.equal(
-          "Response missing one or more of id/rawId/response/type fields"
-        );
-      }
-    });
+  it("should return 'AuthenticatorData is missing'", () => {
+    const body = {
+      id: "id",
+      rawId: "rawId",
+      response: {},
+      type: "public-key"
+    };
+    try {
+      assertionResultReqValidator(body);
+    } catch (e) {
+      expect(e.message).to.equal("AuthenticatorData is missing");
+    }
+  });
 
-    it("should return 'type is not public-key!'", () => {
-      const body = {
-        id: "id",
-        rawId: "rawId",
-        response: {},
-        type: "type"
-      };
-      try {
-        assertionResultReqValidator(body);
-      } catch (e) {
-        expect(e.message).to.equal("type is not public-key!");
-      }
-    });
+  it("should return 'AuthenticatorData is not base64url encoded'", () => {
+    const body = {
+      id: "id",
+      rawId: "rawId",
+      response: {
+        authenticatorData: "+/="
+      },
+      type: "public-key"
+    };
+    try {
+      assertionResultReqValidator(body);
+    } catch (e) {
+      expect(e.message).to.equal("AuthenticatorData is not base64url encoded");
+    }
+  });
 
-    it("should return 'Invalid id!'", () => {
-      const body = {
-        id: "+/=",
-        rawId: "rawId",
-        response: {},
-        type: "public-key"
-      };
-      try {
-        assertionResultReqValidator(body);
-      } catch (e) {
-        expect(e.message).to.equal("Invalid id!");
-      }
-    });
+  it("should return 'userHandle is not of type DOMString'", () => {
+    const body = {
+      id: "id",
+      rawId: "rawId",
+      response: {
+        authenticatorData: "authenticatorData",
+        userHandle: 100
+      },
+      type: "public-key"
+    };
+    try {
+      assertionResultReqValidator(body);
+    } catch (e) {
+      expect(e.message).to.equal("userHandle is not of type DOMString");
+    }
+  });
 
-    it("should return 'AuthenticatorData is missing'", () => {
-      const body = {
-        id: "id",
-        rawId: "rawId",
-        response: {},
-        type: "public-key"
-      };
-      try {
-        assertionResultReqValidator(body);
-      } catch (e) {
-        expect(e.message).to.equal("AuthenticatorData is missing");
-      }
-    });
-
-    it("should return 'AuthenticatorData is not base64url encoded'", () => {
-      const body = {
-        id: "id",
-        rawId: "rawId",
-        response: {
-          authenticatorData: "+/="
-        },
-        type: "public-key"
-      };
-      try {
-        assertionResultReqValidator(body);
-      } catch (e) {
-        expect(e.message).to.equal(
-          "AuthenticatorData is not base64url encoded"
-        );
-      }
-    });
-
-    it("should return 'userHandle is not of type DOMString'", () => {
-      const body = {
-        id: "id",
-        rawId: "rawId",
-        response: {
-          authenticatorData: "authenticatorData",
-          userHandle: 100
-        },
-        type: "public-key"
-      };
-      try {
-        assertionResultReqValidator(body);
-      } catch (e) {
-        expect(e.message).to.equal("userHandle is not of type DOMString");
-      }
-    });
-
-    it("should return 'Signature is not base64url encoded'", () => {
-      const body = {
-        id: "id",
-        rawId: "rawId",
-        response: {
-          authenticatorData: "authenticatorData",
-          userHandle: "userHandle",
-          signature: "+/="
-        },
-        type: "public-key"
-      };
-      try {
-        assertionResultReqValidator(body);
-      } catch (e) {
-        expect(e.message).to.equal("Signature is not base64url encoded");
-      }
-    });
+  it("should return 'Signature is not base64url encoded'", () => {
+    const body = {
+      id: "id",
+      rawId: "rawId",
+      response: {
+        authenticatorData: "authenticatorData",
+        userHandle: "userHandle",
+        signature: "+/="
+      },
+      type: "public-key"
+    };
+    try {
+      assertionResultReqValidator(body);
+    } catch (e) {
+      expect(e.message).to.equal("Signature is not base64url encoded");
+    }
   });
 });
